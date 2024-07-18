9f3531df940323a22f6f3293eb8fe9aa9453b708
Pallavi-Pandey <pallavipandey181@gmail.co>
2024-07-18 12:21:12 +0530

    css changed

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -12,13 +12,24 @@ const Hero = () => {
       <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
         <div className="container  mx-auto">
           <div className='flex justify-between gap-x-8'> 
-            {/*text*/}
+            
             <div className="flex max-w-[680px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
               <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">Web Developer</div>  
-              <h1 className='h1'>Hello, I'm Pallavi Pandey.</h1>
-              <p>
+              <h1 className="h1 mb-4">Hello, I'm Pallavi Pandey.</h1>
+              <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
                 Brief description with insights into myself, my vocational journey, and what I engage in professionally.
               </p>
+              <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
+                <Link href="/contact">
+                  <Button className="gap-x-2">
+                    Contact me<Send size={18} />
+                  </Button>
+                </Link> 
+                <Button variant="secondary" className="gap-x-2">
+                    Download CV
+                    <Download size={18} />
+                </Button> 
+              </div>
             </div>  
             <div className="hidden xl:flex relative">image</div>
           </div>
