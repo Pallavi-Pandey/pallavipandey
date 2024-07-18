a980c093da7751fd326b7c39a848cf8a3c11ea47
Pallavi-Pandey <pallavipandey181@gmail.co>
2024-07-18 12:53:07 +0530

    aded social handels and  updated the css again :) 1:10:14

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+'use client';
 
-const Socials = () => {
+import { RiCodeSSlashFill, RiLinkedinFill, RiGithubFill, RiTerminalFill, RiCommandFill } from "react-icons/ri";
+
+import Link from "next/link";
+
+const icons = [
+  {
+    path: 'https://leetcode.com/Pallavi-Pandey/',
+    name: <RiCodeSSlashFill />
+  },
+  {
+    path: 'www.linkedin.com/in/pallavi-pandey-023',
+    name: <RiLinkedinFill />
+  },
+  {
+    path: 'https://github.com/Pallavi-Pandey',
+    name: <RiGithubFill />
+  },
+  // {
+  //   path: '/',
+  //   name: <RiTerminalFill />
+  // },
+  // {
+  //   path: '/',
+  //   name: <RiCommandFill />
+  // },
+]
+const Socials = ({ containerStyles, iconsStyles }) => {
   return (
-    <div>Socials</div>
+    <div className={`${containerStyles}`}>
+      {icons.map((icon, index) => {
+        return (
+          <Link href={icon.path} key={index}>
+            <div className={`${iconsStyles}`}>{icon.name}</div>
+          </Link>
+        );
+      })}
+    </div>
+
   )
 }
 
