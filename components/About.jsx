ff36915e01f69e6b573f72de770654a802f38e46
Pallavi-Pandey <pallavipandey181@gmail.co>
2024-07-19 10:33:40 +0530

    2:00:57

--- components/About.jsx
+++ components/About.jsx
@@ -9,9 +9,10 @@ import {
     HomeIcon,
     PhoneCall,
     GraduationCap,
-    Calender,
     Briefcase,
+    Calendar,
 } from 'lucide-react';
+
 
 const infoData = [
     {
@@ -35,7 +36,7 @@ const infoData = [
         text: 'B.S in Data Science',
     },
     {
-        icon: <Calender size={20} />,
+        icon: <Calendar size={20} />,
         text: 'Born on 28 Feb, 2001',
     },
 
@@ -116,9 +117,62 @@ const skillData = [
 ]
 
 const About = () => {
-    const
+    const getData = (arr, title) => {
+        return arr.find((item) => item.title === title)
+    }
     return (
-        <div>About</div>
+        <section className="xl:h-[860px] pb-12 xl:py-24">
+            <div className="container mx-auto">
+                <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+                    About me
+                </h2>
+                <div>
+                    {/*image*/}
+                    <div className='hidden xl:flex flex-1 relative'>
+                        <DevImg containerStyles='bg-about_shape_light dark:bg-about_shape_dark w-[500px] h-[500px]
+                    bg-no-repeat relative' imgSrc='/about/developer.png' />
+                    </div>
+                    {/*tabs*/}
+                    <div className='flex-1'>
+                        <Tabs defaultValue="personal">
+                            <TabsList className="w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none">
+                                <TabsTrigger value="personal">Personal Info</TabsTrigger>
+                                <TabsTrigger value="qualifications">Qualifications</TabsTrigger>
+                                <TabsTrigger value="skills">Skills</TabsTrigger>
+                            </TabsList>
+                            <div className="text-lg mt-12 xl:mt-8">
+                                <TabsContent value="personal">
+                                    <div className="text-center xl:text-left">
+                                        <h3 className="h3 mb-4">
+                                            Delivering Unmatched Service Quality for Over a Decade
+                                        </h3>
+                                        <p className="subtitle max-w-xl mx-auto xl:mx-0">
+                                            I specialize in crafting intuitive, user-friendly websites that blend creativity with functionality. With a deep understanding of the latest web technologies and design principles, I strive to create digital experiences that engage and inspire. Whether you're looking to build a new website from scratch or revamp an existing one, my goal is to help you achieve your vision with precision and excellence.
+                                        </p>
+                                        <div className="grid xl:grid-cols-2 gap-4 mb-12">
+                                            {infoData.map((item, index) => {
+                                                return (
+                                                <div 
+                                                className="flex items-center gap-x-4 mx-auto xl:mx-0"
+                                                key={index}>
+                                                    <div className="text-primary">{item.icon}</div>
+                                                    <div>{item.text}</div>
+                                                </div>
+                                                );
+                                            })}
+                                        </div>
+                                    </div>
+                                </TabsContent>
+                                <TabsContent value="qualifications">
+                                    qualifications info
+                                </TabsContent>
+                                <TabsContent value="skills">skills info</TabsContent>
+                            </div>
+                        </Tabs>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
